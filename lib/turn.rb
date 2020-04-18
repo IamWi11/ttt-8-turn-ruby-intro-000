c55165a097a88dee5aa0cf90879e6101a3d1ea80
William <william.l.munoz@gmail.com>
2020-04-18 19:32:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,8 @@ require 'pry'
     user_input = gets.strip
     index = input_to_index(user_input)
     if valid_move?(board, index)
-    eslif turn(board)
+    elsif
+    turn(board)
   end
 end
 binding.binding.pry
