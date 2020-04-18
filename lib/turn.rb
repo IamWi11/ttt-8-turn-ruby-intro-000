6a9d40b41dc13553800f949704e46808a8e2b743
William <william.l.munoz@gmail.com>
2020-04-18 20:05:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def display_board(board)
     user_input = gets.strip
     index = input_to_index(user_input)
     if valid_move?(board, index)
-      display_board(board)
+      return display_board(board)
     else
     turn(board)
   end
